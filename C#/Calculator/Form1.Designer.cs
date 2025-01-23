b467ea1e1183b3b11c946a5a938e8092435497e7
Aiden S. <ASchagren@gmail.com>
2025-01-23 10:32:19 -0800

    Up to Assignment 2.1

---
+++ C#/Calculator/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            CalculatorScreen = new Label();
+            Key1 = new Button();
+            Key2 = new Button();
+            Key3 = new Button();
+            Key6 = new Button();
+            Key5 = new Button();
+            Key4 = new Button();
+            Key9 = new Button();
+            Key8 = new Button();
+            Key7 = new Button();
+            Key0 = new Button();
+            KeyBackSpace = new Button();
+            KeyClear = new Button();
+            KeyDivide = new Button();
+            KeyMultiply = new Button();
+            KeySubtract = new Button();
+            KeyAdd = new Button();
+            NewCalcScreen = new Label();
+            KeyEquals = new Button();
+            SuspendLayout();
+            // 
+            // CalculatorScreen
+            // 
+            CalculatorScreen.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            CalculatorScreen.BackColor = SystemColors.ActiveCaption;
+            CalculatorScreen.Location = new Point(46, 9);
+            CalculatorScreen.MaximumSize = new Size(1000, 1000);
+            CalculatorScreen.Name = "CalculatorScreen";
+            CalculatorScreen.Size = new Size(0, 15);
+            CalculatorScreen.TabIndex = 0;
+            CalculatorScreen.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Key1
+            // 
+            Key1.BackColor = SystemColors.ActiveCaption;
+            Key1.Location = new Point(46, 144);
+            Key1.Name = "Key1";
+            Key1.Size = new Size(100, 55);
+            Key1.TabIndex = 1;
+            Key1.Text = "1";
+            Key1.UseVisualStyleBackColor = false;
+            Key1.Click += Key1_Click;
+            // 
+            // Key2
+            // 
+            Key2.BackColor = SystemColors.ActiveCaption;
+            Key2.Location = new Point(224, 144);
+            Key2.Name = "Key2";
+            Key2.Size = new Size(100, 55);
+            Key2.TabIndex = 2;
+            Key2.Text = "2";
+            Key2.UseVisualStyleBackColor = false;
+            Key2.Click += Key2_Click;
+            // 
+            // Key3
+            // 
+            Key3.BackColor = SystemColors.ActiveCaption;
+            Key3.Location = new Point(416, 144);
+            Key3.Name = "Key3";
+            Key3.Size = new Size(100, 55);
+            Key3.TabIndex = 3;
+            Key3.Text = "3";
+            Key3.UseVisualStyleBackColor = false;
+            Key3.Click += Key3_Click;
+            // 
+            // Key6
+            // 
+            Key6.BackColor = SystemColors.ActiveCaption;
+            Key6.Location = new Point(416, 225);
+            Key6.Name = "Key6";
+            Key6.Size = new Size(100, 55);
+            Key6.TabIndex = 6;
+            Key6.Text = "6";
+            Key6.UseVisualStyleBackColor = false;
+            Key6.Click += Key6_Click;
+            // 
+            // Key5
+            // 
+            Key5.BackColor = SystemColors.ActiveCaption;
+            Key5.Location = new Point(224, 225);
+            Key5.Name = "Key5";
+            Key5.Size = new Size(100, 55);
+            Key5.TabIndex = 5;
+            Key5.Text = "5";
+            Key5.UseVisualStyleBackColor = false;
+            Key5.Click += Key5_Click;
+            // 
+            // Key4
+            // 
+            Key4.BackColor = SystemColors.ActiveCaption;
+            Key4.Location = new Point(46, 225);
+            Key4.Name = "Key4";
+            Key4.Size = new Size(100, 55);
+            Key4.TabIndex = 4;
+            Key4.Text = "4";
+            Key4.UseVisualStyleBackColor = false;
+            Key4.Click += Key4_Click;
+            // 
+            // Key9
+            // 
+            Key9.BackColor = SystemColors.ActiveCaption;
+            Key9.Location = new Point(416, 306);
+            Key9.Name = "Key9";
+            Key9.Size = new Size(100, 55);
+            Key9.TabIndex = 9;
+            Key9.Text = "9";
+            Key9.UseVisualStyleBackColor = false;
+            Key9.Click += Key9_Click;
+            // 
+            // Key8
+            // 
+            Key8.BackColor = SystemColors.ActiveCaption;
+            Key8.Location = new Point(224, 306);
+            Key8.Name = "Key8";
+            Key8.Size = new Size(100, 55);
+            Key8.TabIndex = 8;
+            Key8.Text = "8";
+            Key8.UseVisualStyleBackColor = false;
+            Key8.Click += Key8_Click;
+            // 
+            // Key7
+            // 
+            Key7.BackColor = SystemColors.ActiveCaption;
+            Key7.Location = new Point(46, 306);
+            Key7.Name = "Key7";
+            Key7.Size = new Size(100, 55);
+            Key7.TabIndex = 7;
+            Key7.Text = "7";
+            Key7.UseVisualStyleBackColor = false;
+            Key7.Click += Key7_Click;
+            // 
+            // Key0
+            // 
+            Key0.BackColor = SystemColors.ActiveCaption;
+            Key0.Location = new Point(224, 383);
+            Key0.Name = "Key0";
+            Key0.Size = new Size(100, 55);
+            Key0.TabIndex = 10;
+            Key0.Text = "0";
+            Key0.UseVisualStyleBackColor = false;
+            Key0.Click += Key0_Click;
+            // 
+            // KeyBackSpace
+            // 
+            KeyBackSpace.BackColor = SystemColors.ActiveCaption;
+            KeyBackSpace.Location = new Point(416, 383);
+            KeyBackSpace.Name = "KeyBackSpace";
+            KeyBackSpace.Size = new Size(100, 55);
+            KeyBackSpace.TabIndex = 12;
+            KeyBackSpace.Text = "<<";
+            KeyBackSpace.UseVisualStyleBackColor = false;
+            KeyBackSpace.Click += KeyBackSpace_Click;
+            // 
+            // KeyClear
+            // 
+            KeyClear.BackColor = SystemColors.ActiveCaption;
+            KeyClear.Location = new Point(46, 383);
+            KeyClear.Name = "KeyClear";
+            KeyClear.Size = new Size(100, 55);
+            KeyClear.TabIndex = 11;
+            KeyClear.Text = "A/C";
+            KeyClear.UseVisualStyleBackColor = false;
+            KeyClear.Click += KeyClear_Click;
+            // 
+            // KeyDivide
+            // 
+            KeyDivide.BackColor = SystemColors.ActiveCaption;
+            KeyDivide.Location = new Point(615, 255);
+            KeyDivide.Name = "KeyDivide";
+            KeyDivide.Size = new Size(100, 75);
+            KeyDivide.TabIndex = 16;
+            KeyDivide.Text = "/";
+            KeyDivide.UseVisualStyleBackColor = false;
+            KeyDivide.Click += KeyDivide_Click;
+            // 
+            // KeyMultiply
+            // 
+            KeyMultiply.BackColor = SystemColors.ActiveCaption;
+            KeyMultiply.Location = new Point(615, 174);
+            KeyMultiply.Name = "KeyMultiply";
+            KeyMultiply.Size = new Size(100, 75);
+            KeyMultiply.TabIndex = 15;
+            KeyMultiply.Text = "*";
+            KeyMultiply.UseVisualStyleBackColor = false;
+            KeyMultiply.Click += KeyMultiply_Click;
+            // 
+            // KeySubtract
+            // 
+            KeySubtract.BackColor = SystemColors.ActiveCaption;
+            KeySubtract.Location = new Point(615, 93);
+            KeySubtract.Name = "KeySubtract";
+            KeySubtract.Size = new Size(100, 75);
+            KeySubtract.TabIndex = 14;
+            KeySubtract.Text = "-";
+            KeySubtract.UseVisualStyleBackColor = false;
+            KeySubtract.Click += KeySubtract_Click;
+            // 
+            // KeyAdd
+            // 
+            KeyAdd.BackColor = SystemColors.ActiveCaption;
+            KeyAdd.Location = new Point(615, 12);
+            KeyAdd.Name = "KeyAdd";
+            KeyAdd.Size = new Size(100, 75);
+            KeyAdd.TabIndex = 13;
+            KeyAdd.Text = "+";
+            KeyAdd.UseVisualStyleBackColor = false;
+            KeyAdd.Click += KeyAdd_Click;
+            // 
+            // NewCalcScreen
+            // 
+            NewCalcScreen.BackColor = SystemColors.ActiveCaption;
+            NewCalcScreen.Location = new Point(46, 9);
+            NewCalcScreen.Name = "NewCalcScreen";
+            NewCalcScreen.Size = new Size(470, 123);
+            NewCalcScreen.TabIndex = 17;
+            NewCalcScreen.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // KeyEquals
+            // 
+            KeyEquals.BackColor = SystemColors.ActiveCaption;
+            KeyEquals.Location = new Point(615, 363);
+            KeyEquals.Name = "KeyEquals";
+            KeyEquals.Size = new Size(100, 75);
+            KeyEquals.TabIndex = 18;
+            KeyEquals.Text = "=";
+            KeyEquals.UseVisualStyleBackColor = false;
+            KeyEquals.Click += KeyEquals_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(128, 128, 255);
+            ClientSize = new Size(800, 450);
+            Controls.Add(KeyEquals);
+            Controls.Add(NewCalcScreen);
+            Controls.Add(KeyDivide);
+            Controls.Add(KeyMultiply);
+            Controls.Add(KeySubtract);
+            Controls.Add(KeyAdd);
+            Controls.Add(KeyBackSpace);
+            Controls.Add(KeyClear);
+            Controls.Add(Key0);
+            Controls.Add(Key9);
+            Controls.Add(Key8);
+            Controls.Add(Key7);
+            Controls.Add(Key6);
+            Controls.Add(Key5);
+            Controls.Add(Key4);
+            Controls.Add(Key3);
+            Controls.Add(Key2);
+            Controls.Add(Key1);
+            Controls.Add(CalculatorScreen);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label CalculatorScreen;
+        private Button Key1;
+        private Button Key2;
+        private Button Key3;
+        private Button Key6;
+        private Button Key5;
+        private Button Key4;
+        private Button Key9;
+        private Button Key8;
+        private Button Key7;
+        private Button Key0;
+        private Button KeyBackSpace;
+        private Button KeyClear;
+        private Button KeyDivide;
+        private Button KeyMultiply;
+        private Button KeySubtract;
+        private Button KeyAdd;
+        private Label NewCalcScreen;
+        private Button KeyEquals;
+    }
+}
---
+++ C#/Calculator/Form1.cs
@@ -0,0 +1,308 @@
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            valuesnfactors.Add(""); //Adds to the list on initialization for functionality purposes
+        }
+
+        //Variables
+
+        List<string> valuesnfactors = new List<string>(); //List of all values and math signs
+        int currentslot = 0; //Current slot in the list of values and signs
+
+        bool needsclearing = false; //Failsafe, if the player did a calculation, they need to press A/C before proceeding.
+
+        private void KeyClear_Click(object sender, EventArgs e) //Completely clears the current input of strings in the index, resets it, and resets the failsafe and current index
+        {
+            valuesnfactors.Clear();
+            valuesnfactors.Add("");
+            needsclearing = false;
+            currentslot = 0;
+            updateUI();
+        }
+
+        #region Keypresses 0-9
+
+        private void Key1_Click(object sender, EventArgs e) //Adds a 1 to the current index value
+        {
+            if (needsclearing) return;
+            valuesnfactors[currentslot] += "1";
+            updateUI();
+        }
+
+        private void Key2_Click(object sender, EventArgs e) //Adds a 2 to the current index value
+        {
+            if (needsclearing) return;
+            valuesnfactors[currentslot] += "2";
+            updateUI();
+        }
+
+        private void Key3_Click(object sender, EventArgs e) //Adds a 3 to the current index value
+        {
+            if (needsclearing) return;
+            valuesnfactors[currentslot] += "3";
+            updateUI();
+        }
+
+        private void Key4_Click(object sender, EventArgs e) //Adds a 4 to the current index value
+        {
+            if (needsclearing) return;
+            valuesnfactors[currentslot] += "4";
+            updateUI();
+        }
+
+        private void Key5_Click(object sender, EventArgs e) //Adds a 5 to the current index value
+        {
+            if (needsclearing) return;
+            valuesnfactors[currentslot] += "5";
+            updateUI();
+        }
+
+        private void Key6_Click(object sender, EventArgs e) //Adds a 6 to the current index value
+        {
+            if (needsclearing) return;
+            valuesnfactors[currentslot] += "6";
+            updateUI();
+        }
+
+        private void Key7_Click(object sender, EventArgs e) //Adds a 7 to the current index value
+        {
+            if (needsclearing) return;
+            valuesnfactors[currentslot] += "7";
+            updateUI();
+        }
+
+        private void Key8_Click(object sender, EventArgs e) //Adds a 8 to the current index value
+        {
+            if (needsclearing) return;
+            valuesnfactors[currentslot] += "8";
+            updateUI();
+        }
+
+        private void Key9_Click(object sender, EventArgs e) //Adds a 9 to the current index value
+        {
+            if (needsclearing) return;
+            valuesnfactors[currentslot] += "9";
+            updateUI();
+        }
+
+        private void Key0_Click(object sender, EventArgs e) //Adds a 0 to the current index value
+        {
+            if (needsclearing) return;
+            valuesnfactors[currentslot] += "0";
+            updateUI();
+        }
+
+        private void KeyBackSpace_Click(object sender, EventArgs e) //Removes items from index and from values of the current selection
+        {
+            if (needsclearing) return; //Ensures program doesn't need a reset
+
+            if (valuesnfactors[currentslot] == "" && valuesnfactors.Count > 2) //Checks if the current index number is empty and if there is currently more than one number in the list
+            {
+                valuesnfactors.RemoveAt(currentslot); //Removes the number slot
+                valuesnfactors.RemoveAt(currentslot - 1); //Removes the mathematical sign slot
+                currentslot -= 2; //Moves down two in the index
+                updateUI();
+            }
+            else
+            {
+                if (valuesnfactors[currentslot].Length == 0) //Checks if the current index character count is equal to zero, if not, it will remove the last character
+                {
+                    MessageBox.Show("Value to short");
+                    return;
+                }
+                else
+                {
+                    //First thing researched online, removing the last item from a string, forgot it was a two step process, using the string as a local variable then setting it
+
+                    string removed = valuesnfactors[currentslot].Substring(0, valuesnfactors[currentslot].Length - 1); // Creates a substring from the current string without the last set digit
+                    valuesnfactors[currentslot] = removed; // Sets the current arranged value without that deleted number
+                    updateUI();
+                }
+
+            }
+        }
+
+        #endregion
+
+        #region Math Signs
+        private void KeyAdd_Click(object sender, EventArgs e)
+        {
+            if (needsclearing) return; //Ensures program doesn't need a reset
+
+            valuesnfactors.Add("+");
+            valuesnfactors.Add("");
+            currentslot += 2;
+            updateUI();
+        }
+
+        private void KeySubtract_Click(object sender, EventArgs e)
+        {
+            if (needsclearing) return; //Ensures program doesn't need a reset
+
+            valuesnfactors.Add("-");
+            valuesnfactors.Add("");
+            currentslot += 2;
+            updateUI();
+        }
+
+        private void KeyMultiply_Click(object sender, EventArgs e)
+        {
+            if (needsclearing) return; //Ensures program doesn't need a reset
+
+            valuesnfactors.Add("*");
+            valuesnfactors.Add("");
+            currentslot += 2;
+            updateUI();
+        }
+
+        private void KeyDivide_Click(object sender, EventArgs e)
+        {
+            if (needsclearing) return; //Ensures program doesn't need a reset
+
+            valuesnfactors.Add("/");
+            valuesnfactors.Add("");
+            currentslot += 2;
+            updateUI();
+        }
+
+        #endregion
+
+        private void KeyEquals_Click(object sender, EventArgs e)
+        {
+            if (needsclearing) return;
+            if (valuesnfactors.Count <= 1) return;
+            needsclearing = true;
+            MathCalculation();
+        }
+
+        void MathCalculation()
+        {
+            //Second thing researched for this work, Inserting a value at a certain spot of index in a list. Learned that it is (listname).Insert(index of insertion, what's being added)
+
+
+            #region Sign Checker
+
+            //Correct Version, checking the signs in an order before moving to the next
+
+            if (valuesnfactors.Contains("*")) //Checks if the index has any * signs
+            {
+                for(int i = 0; i < valuesnfactors.Count; i++)
+                {
+                    if (valuesnfactors[i] == "*")
+                    {
+                        int multVal = Convert.ToInt32(valuesnfactors[i - 1]) * Convert.ToInt32(valuesnfactors[i + 1]); //Get the Multiplied Value of the number indexes left and right of the identified of the sign
+
+
+                        valuesnfactors.RemoveAt(i + 1); //Removes the second calculated number, sign, and then first calculated number from the registry in that order (Via RemoveAt)
+
+                        valuesnfactors.Insert(i + 1, multVal.ToString()); //Adds the newly multiplied value to the index the second value in this equation was
+
+                        valuesnfactors.RemoveAt(i);
+                        valuesnfactors.RemoveAt(i - 1);
+
+                        CheckforOutput(); //Checks the loop
+                        return; //Breaks the loop
+                    }
+                }
+            }
+
+            if (valuesnfactors.Contains("/")) //Checks if the index has and / signs
+            {
+                for(int i = 0; i < valuesnfactors.Count; i++)
+                {
+                    //Third thing with online research, changing a float to an int.. its stupid. First make a local float using the values in the equation, the changing it with the (int) tag next to the float
+
+                    if(valuesnfactors[i] == "/")
+                    {
+                        float needtoRound = Convert.ToInt32(valuesnfactors[i - 1]) / Convert.ToInt32(valuesnfactors[i + 1]); //Get the float Divided Value of the number indexes left and right of the identified of the sign
+                        int divVal = (int)needtoRound; //Sets the float as an integer within the equation
+
+
+                        valuesnfactors.RemoveAt(i + 1); //Removes the second calculated number, sign, and then first calculated number from the registry in that order (Via RemoveAt)
+
+                        valuesnfactors.Insert(i + 1, divVal.ToString()); //Adds the newly divided value to the index the second value in this equation was
+
+                        valuesnfactors.RemoveAt(i);
+                        valuesnfactors.RemoveAt(i - 1);
+
+                        CheckforOutput(); //Checks the loop
+                        return; //Breaks the loop
+                    }
+                }
+            }
+
+            if (valuesnfactors.Contains("+")) //Checks if the index has any + signs
+            {
+                for(int i = 0;i < valuesnfactors.Count; i++)
+                {
+                    if (valuesnfactors[i] == "+")
+                    {
+                        int addVal = Convert.ToInt32(valuesnfactors[i - 1]) + Convert.ToInt32(valuesnfactors[i + 1]); //Get the Added Value of the number indexes left and right of the identified of the sign
+
+
+                        valuesnfactors.RemoveAt(i + 1); //Removes the second calculated number, sign, and then first calculated number from the registry in that order (Via RemoveAt)
+
+                        valuesnfactors.Insert(i + 1, addVal.ToString()); //Adds the newly added value to the index the second value in this equation was
+
+                        valuesnfactors.RemoveAt(i);
+                        valuesnfactors.RemoveAt(i - 1);
+
+                        CheckforOutput(); //Checks the loop
+                        return; //Breaks the loop
+                    }
+                }
+            }
+
+            if (valuesnfactors.Contains("-")) //Checks if the index has and - signs
+            {
+                for(int i = 0; i < valuesnfactors.Count; i++)
+                {
+                    if(valuesnfactors[i] == "-")
+                    {
+                        int subVal = Convert.ToInt32(valuesnfactors[i - 1]) - Convert.ToInt32(valuesnfactors[i + 1]); //Get the Subtracted Value of the number indexes left and right of the identified of the sign
+
+
+                        valuesnfactors.RemoveAt(i + 1); //Removes the second calculated number, sign, and then first calculated number from the registry in that order (Via RemoveAt)
+
+                        valuesnfactors.Insert(i + 1, subVal.ToString()); //Adds the newly subtracted value to the index the second value in this equation was
+
+                        valuesnfactors.RemoveAt(i);
+                        valuesnfactors.RemoveAt(i - 1);
+
+                        CheckforOutput(); //Checks the loop
+                        return; //Breaks the loop
+                    }
+                }
+            }
+            
+        }
+
+        #endregion
+
+        void CheckforOutput() //Checks if the index only has one remaining value, if it doesn't, it will run the Math Calculation again until there are only one in the index
+        {
+            if (valuesnfactors.Count == 1)
+            {
+                updateUI();
+            }
+            else
+            {
+                MathCalculation();
+            }
+        }
+
+        void updateUI()
+        {
+            NewCalcScreen.Text = "";
+
+            for (int i = 0; i < valuesnfactors.Count; i++)
+            {
+                NewCalcScreen.Text += valuesnfactors[i] + " ";
+            }
+        }
+    }
+}
